a2a046a0b89fe0989dad9777344d03aa1999765a
Sara Haines <schaines@wisc.edu>
2021-09-22 13:11:47 -0500

    Milestone2 complete

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.lab2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,57 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void goToActivity2(int result){
+        Intent intent = new Intent(this, MainActivity2.class);
+        intent.putExtra("result", Integer.toString(result));
+        startActivity(intent);
+    }
+
+    public void addition(View view) {
+        EditText firstText = (EditText) findViewById(R.id.editTextNumber1);
+        int firstNumber = Integer.parseInt(firstText.getText().toString());
+
+        EditText secondText = (EditText) findViewById(R.id.editTextNumber2);
+        int secondNumber = Integer.parseInt(secondText.getText().toString());
+
+        int result = firstNumber + secondNumber;
+        goToActivity2(result);
+
+    }
+
+    public void subtraction(View view) {
+        EditText firstText = (EditText) findViewById(R.id.editTextNumber1);
+        int firstNumber = Integer.parseInt(firstText.getText().toString());
+
+        EditText secondText = (EditText) findViewById(R.id.editTextNumber2);
+        int secondNumber = Integer.parseInt(secondText.getText().toString());
+
+        int result = firstNumber - secondNumber;
+        goToActivity2(result);
+    }
+
+    public void multiplication(View view) {
+        EditText firstText = (EditText) findViewById(R.id.editTextNumber1);
+        int firstNumber = Integer.parseInt(firstText.getText().toString());
+
+        EditText secondText = (EditText) findViewById(R.id.editTextNumber2);
+        int secondNumber = Integer.parseInt(secondText.getText().toString());
+
+        int result = firstNumber * secondNumber;
+        goToActivity2(result);
+    }
+
+    public void division(View view) {
+        EditText firstText = (EditText) findViewById(R.id.editTextNumber1);
+        int firstNumber = Integer.parseInt(firstText.getText().toString());
+
+        EditText secondText = (EditText) findViewById(R.id.editTextNumber2);
+        int secondNumber = Integer.parseInt(secondText.getText().toString());
+
+        int result = firstNumber / secondNumber;
+        goToActivity2(result);
     }
 }
